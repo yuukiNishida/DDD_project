8cc7bbee523252f2fd30d34e2c4bc2a4e896de86
yuuki_nishida <yuuki.nishida@bcmembers.com>
2018-08-10 22:12:32 +0900

    初期コミット

---
+++ Assets/Resources/Script/Performance.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemUtility : MonoBehaviour {
+
+	void Start () {
+		OVRManager.display.displayFrequency = 72f; //72Hzモードに変更
+
+		//パフォーマンスレベルの変更 Default=2 0~3,低~高 とパフォーマンスが変わる
+		//3にすると高クロックになり著しくバッテリー消費と発熱を起こす．
+		OVRManager.cpuLevel = 2;
+		OVRManager.gpuLevel = 2;
+	}
+}
---
+++ Assets/Script/anim_script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anim_script : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		Animation anim = GetComponent<Animation> ();
+        anim.Play();
+    }
+
+}
